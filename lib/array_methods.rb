ceb8107ae95edf04cfd93d2229551999e22615ce
Ferney Arenas <arenas.mauricio@yahoo.com>
2020-01-20 12:50:28 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,18 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+ scale = [2,3,4,5]
+ do
+expect(find_element_index(scale,2)).to eq(0)
+end
 end
 
 def find_max_value(array)
-  # Add your solution here
+  do
+      expect(find_max_value(scale)).to eq(5)
+    end
 end
 
 def find_min_value(array)
-  # Add your solution here
+  do
+      expect(find_min_value(scale)).to eq(2)
+    end
 end
